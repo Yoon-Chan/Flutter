165db9b8550c522bda929242b43875ac208b14c1
chan <vsvx13@naver.com>
2023-01-26 15:59:30 +0900

    이미지 선택 후 화면에 보여주기 기능, 탭 이미지 선택 구현

--- Flutter/image_editor/lib/screen/home_screen.dart
+++ Flutter/image_editor/lib/screen/home_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_editor/component/main_app_bar.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
 
   const HomeScreen(
       {
@@ -10,11 +13,24 @@ class HomeScreen extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreen();
+
+}
+
+
+class _HomeScreen extends State<HomeScreen>{
+
+  XFile? image; //선택한 이미지를 저장할 변수
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,// 자식 위젯 최대 크기로 펼치기
         children: [
+
+          renderBody(),
+
           //MainAppBar를 좌, 우 , 위 끝에 정렬
           Positioned(
               top: 0,
@@ -32,9 +48,44 @@ class HomeScreen extends StatelessWidget {
   }
 
 
-  void onPickImage(){}
+  void onPickImage() async {
+
+    //갤러리에서 이미지 선택
+    final image = await ImagePicker()
+        .pickImage(source: ImageSource.gallery);
+
+
+    setState(() {
+      this.image = image;
+    });
+  }
   void onDeleteItem() {}
   void onSaveImage() {}
 
+
+
+  Widget renderBody(){
+    if (image != null){
+      // Stack 크기의 최대 크기만큼 차지하기
+      return Positioned.fill(
+          //위젯 확대 및 좌우 이동을 가능하게 하는 위젯
+          child: InteractiveViewer(
+            child: Image.file(File(image!.path),
+              fit: BoxFit.cover,
+            ),
+          ) );
+    } else {
+      //이미지 선택이 안 된 경우 이미지 선택 버튼 표시
+      return Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            primary: Colors.grey,
+          ),
+          onPressed: onPickImage,
+          child: Text("이미지 선택하기"),
+        ),
+      );
+    }
+  }
 
 }
