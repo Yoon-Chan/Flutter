7637b137f45ad7b8280dc5e0ff51e6c3d27e1b59
chan <vsvx13@naver.com>
2023-02-01 16:44:12 +0900

    내부 저장소를 이용하여 파일에 있는 fruit.txt를 읽어 ListView에 보여주고 또한 TextEditting과 TextField를 이용하여 작성한 글을 추가하고 파일에 저장하는 기능

--- Doit/fileio_example/lib/fileApp.dart
+++ Doit/fileio_example/lib/fileApp.dart
@@ -1,27 +1,70 @@
-
-
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class FileApp extends StatefulWidget{
-
+class FileApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _FileApp();
-
 }
 
 class _FileApp extends State<FileApp> {
   int _count = 0;
+  List<String> itemList = new List.empty(growable: true);
+  TextEditingController controller = new TextEditingController();
+
+  Future<List<String>> readListFile() async {
+    List<String> itemList = new List.empty(growable: true);
+
+    var key = 'first';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+
+    bool? firstCheck = pref.getBool(key);
+
+    var dir = await getApplicationDocumentsDirectory();
+
+    bool fileExist = await File(dir.path + '/repo/fruit.txt').exists();
+
+    if (firstCheck == null || firstCheck == false || fileExist == false) {
+      pref.setBool(key, true);
+      var file =
+          await DefaultAssetBundle.of(context).loadString('repo/fruit.txt');
+
+      File(dir.path + '/fruit.txt').writeAsStringSync(file);
+
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+      return itemList;
+    } else {
+      var file = await File(dir.path + '/fruit.txt').readAsString();
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+
+      return itemList;
+    }
+  }
+
+  void initData() async {
+    var result = await readListFile();
+    setState(() {
+      itemList.addAll(result);
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     readCountFile();
+    initData();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -31,27 +74,43 @@ class _FileApp extends State<FileApp> {
       ),
       body: Container(
         child: Center(
-          child: Text(
-            '$_count',
-            style: TextStyle(fontSize: 40),
+          child: Column(
+            children: [
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.text,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: itemList.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Center(
+                        child: Text(
+                          itemList[index],
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
           ),
         ),
       ),
-
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          writeFruit(controller.value.text);
           setState(() {
-            _count++;
+            itemList.add(controller.value.text);
           });
 
-          writeCountFile(_count);
         },
         child: Icon(Icons.add),
       ),
     );
-
   }
-
 
   void writeCountFile(int count) async {
     var dir = await getApplicationDocumentsDirectory();
@@ -67,8 +126,17 @@ class _FileApp extends State<FileApp> {
       setState(() {
         _count = int.parse(file);
       });
-    }catch(e) {
+    } catch (e) {
       print(e.toString());
     }
+  }
+
+
+  void writeFruit(String fruit) async {
+    var dir = await getApplicationDocumentsDirectory();
+    var file = await File(dir.path + '/fruit.txt').readAsString();
+    file = file + '\n' + fruit;
+    File(dir.path + '/fruit.txt').writeAsStringSync(file);
+
   }
 }
