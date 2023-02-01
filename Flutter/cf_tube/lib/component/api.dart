d323a06d1a90bd60ade3ea2e2043a0159cf63ccd
Yoon-Chan <56026214+Yoon-Chan@users.noreply.github.com>
2023-02-01 12:39:43 +0900

    Update api.dart

--- Flutter/cf_tube/lib/component/api.dart
+++ Flutter/cf_tube/lib/component/api.dart
@@ -1,4 +1,4 @@
 
-const API_KEY = 'AIzaSyD9hrk-GpB6ht2Z4JGOG2nDymqDT2IJ6Yo';
+const API_KEY = '';
 const YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 const CF_CHANNEL_ID = 'UC17ItE3c0MZ5CyHumJXWn7w';
