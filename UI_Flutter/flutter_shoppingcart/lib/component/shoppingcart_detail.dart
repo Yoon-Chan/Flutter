55ff6e92ebe170b014bc3e87ecd5cb2ac0a382ac
chan <vsvx13@naver.com>
2023-02-11 14:47:58 +0900

    쇼핑카트 UI 앱 - Detail 부분 추가

--- UI_Flutter/flutter_shoppingcart/lib/component/shoppingcart_detail.dart
+++ UI_Flutter/flutter_shoppingcart/lib/component/shoppingcart_detail.dart
@@ -1,10 +1,163 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_shoppingcart/constants.dart';
 
 class ShoppingCartDetail extends StatelessWidget {
   const ShoppingCartDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDetailNameAndPrice(),
+            _buildDetailRatingAndReviewCount(),
+            _buildDetailColorOptions(),
+            _buildDetailButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailNameAndPrice() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Urban Soft AL 10.0",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "\$699",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailRatingAndReviewCount() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          //Spacer() 로 Icon 위젯과 Text위젯을 양 끝으로 벌릴 수 있따. spaceBetween과 동일
+          Spacer(),
+          Text("review"),
+          Text(
+            "(26)",
+            style: TextStyle(color: Colors.blue),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailColorOptions() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Color Options"),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              _buildDetailIcon(Colors.black),
+              _buildDetailIcon(Colors.green),
+              _buildDetailIcon(Colors.orange),
+              _buildDetailIcon(Colors.grey),
+              _buildDetailIcon(Colors.white),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailIcon(Color color) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10),
+      child: Stack(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(),
+              shape: BoxShape.circle,
+            ),
+          ),
+          Positioned(
+              left: 5,
+              top: 5,
+              child: ClipOval(
+                child: Container(
+                  color: color,
+                  width: 40,
+                  height: 40,
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailButton(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+            backgroundColor: KAccentColor,
+            minimumSize: Size(300, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            )),
+        child: Text(
+          "Add to Cart",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
   }
 }
