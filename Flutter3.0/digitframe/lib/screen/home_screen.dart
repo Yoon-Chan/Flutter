62a705add29fb2c921aa35b9c97636682a6e62dd
윤찬 <vsvx13@naver.com>
2024-05-18 00:18:37 +0900

    Feat:[digitFrame] PagerView 사용하기

---
+++ Flutter3.0/digitframe/lib/screen/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: PageView(
+          children: [1, 2, 3, 4, 5]
+              .map((e) => Image.asset('asset/img/image_$e.jpeg', fit: BoxFit.cover,))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
