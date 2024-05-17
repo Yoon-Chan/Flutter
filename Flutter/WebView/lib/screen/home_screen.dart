eec2836ab3b6ce40a3680eded492af92cb2f1b89
윤찬 <vsvx13@naver.com>
2024-05-17 23:48:40 +0900

    Feat:[digitFrame] init project

--- Flutter/WebView/lib/screen/home_screen.dart
+++ Flutter/WebView/lib/screen/home_screen.dart
@@ -25,20 +25,23 @@ class HomeScreen extends StatelessWidget {
                   controller!.loadUrl("https://blog.codefactory.ai");
                 }
               },
-              icon: Icon(Icons.home,)),
-          IconButton(onPressed: () {
-            if (controller != null) {
-              controller!.goBack();
-            }
-          }
-              , icon: Icon(Icons.skip_previous_sharp)),
-
-          IconButton(onPressed: () {
-            if (controller != null) {
-              controller!.goForward();
-            }
-          }
-              , icon: Icon(Icons.skip_next_sharp)),
+              icon: Icon(
+                Icons.home,
+              )),
+          IconButton(
+              onPressed: () {
+                if (controller != null) {
+                  controller!.goBack();
+                }
+              },
+              icon: Icon(Icons.skip_previous_sharp)),
+          IconButton(
+              onPressed: () {
+                if (controller != null) {
+                  controller!.goForward();
+                }
+              },
+              icon: Icon(Icons.skip_next_sharp)),
         ],
       ),
       body: WebView(
@@ -47,6 +50,43 @@ class HomeScreen extends StatelessWidget {
         onWebViewCreated: (WebViewController controller) {
           this.controller = controller; //위젯에 컨트롤러 저장
         },
+      ),
+    );
+  }
+}
+
+class FloatingActionButtonExample extends StatelessWidget {
+  const FloatingActionButtonExample({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          //클릭했을 때 실행할 함수
+          onPressed: () {},
+          child: Text('클릭'),
+        ),
+        body: Container(
+          //스타일 적용
+          decoration: BoxDecoration(
+            //배경 색깔 적용
+            color: Colors.red,
+            //테두리 적용
+            border: Border.all(
+                //테두리 굵기
+                width: 16.0,
+                //테두리 색상
+                color: Colors.black),
+            //모서리 라운드 처리
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          width: 200.0,
+          height: 100.0,
+
+        ),
       ),
     );
   }
