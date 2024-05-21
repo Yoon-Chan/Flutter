fcdaa77cb14d369c65eddbe724fbf1fbcb7aaa1a
윤찬 <vsvx13@naver.com>
2024-05-21 16:57:33 +0900

    Feat:[calendar_scheduler] 일정 카드 구현

--- Flutter3.0/calendar_scheduler/lib/screen/home_screen.dart
+++ Flutter3.0/calendar_scheduler/lib/screen/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:calendar_scheduler/component/calendar.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:calendar_scheduler/const/color.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -11,7 +14,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime? selectedDay;
+  DateTime selectedDay = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -19,14 +26,34 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Container(
             child: Column(
-              children: [
-                Calendar(
-                  focusedDay: DateTime.now(),
-                  onDaySelected: onDaySelected,
-                  selectedDayPredicate: selectedDayPredicate,
-                )
-              ],
-            )),
+          children: [
+            Calendar(
+              focusedDay: DateTime.now(),
+              onDaySelected: onDaySelected,
+              selectedDayPredicate: selectedDayPredicate,
+            ),
+            TodayBanner(
+              selectedDay: selectedDay,
+              taskCount: 0,
+            ),
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
+                child: ListView(
+                  children: [
+                    ScheduleCard(
+                      startTime: DateTime(2024, 05, 21, 11),
+                      endTime: DateTime(2024, 05, 21, 12),
+                      content: '플러터 공부하기',
+                      color: Colors.blue,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
@@ -41,5 +68,4 @@ class _HomeScreenState extends State<HomeScreen> {
     if (selectedDay == null) return false;
     return day.isAtSameMomentAs(selectedDay!);
   }
-
 }
