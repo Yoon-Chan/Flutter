9aa13eebf1a70a94975eed0aac807be857ab497a
윤찬 <vsvx13@naver.com>
2024-05-21 14:03:35 +0900

    Update home_screen.dart

--- Flutter3.0/digitframe/lib/screen/home_screen.dart
+++ Flutter3.0/digitframe/lib/screen/home_screen.dart
@@ -1,18 +1,52 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  Timer? timer;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(Duration(seconds: 2), (timer) {
+      int currentPage = controller.page == null ? 0 : controller.page!.toInt();
+      int nextPage = (currentPage + 1) % 4;
+
+      controller.animateToPage(
+          nextPage,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.linear);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: PageView(
+          controller: controller,
           children: [1, 2, 3, 4, 5]
-              .map((e) => Image.asset('asset/img/image_$e.jpeg', fit: BoxFit.cover,))
+              .map((e) =>
+              Image.asset('asset/img/image_$e.jpeg', fit: BoxFit.cover,))
               .toList(),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    timer?.cancel();
+    controller.dispose();
   }
 }
