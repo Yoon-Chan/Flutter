46a2f187fab44b76aad5424e8b3c8f6d7090e306
윤찬 <vsvx13@naver.com>
2024-05-21 22:41:45 +0900

    Feat:[calendar_scheduler] drift를 이용해 schedules 테이블 변경

--- Flutter3.0/calendar_scheduler/lib/model/schedule.dart
+++ Flutter3.0/calendar_scheduler/lib/model/schedule.dart
@@ -1,32 +1,30 @@
-class Schedules {
+import 'package:drift/drift.dart';
+
+class Schedules extends Table{
   // 1) 식별 가능한 Id
-  final int id;
+  // final int id;
+  IntColumn get id => integer().autoIncrement()();
 
   // 2) 시작 시간
-  final int startTime;
-
+  // final int startTime;
+  IntColumn get startTime => integer()();
   // 3) 종료 시간
-  final int endTime;
-
+  // final int endTime;
+  IntColumn get endTime => integer()();
   // 4) 일정 내용
-  final String content;
-
+  // final String content;
+  TextColumn get content => text()();
   // 5) 날짜
-  final DateTime date;
+  // final DateTime date;
+  DateTimeColumn get date => dateTime()();
 
   // 6) 카테고리
-  final String color;
+  // final String color;
+  TextColumn get color => text()();
 
   // 7) 일정 생성날짜 시간
-  final DateTime createdAt;
+  // final DateTime createdAt;
+  DateTimeColumn get createdAt => dateTime().clientDefault(
+          () => DateTime.now().toUtc())();
 
-  Schedules({
-    required this.id,
-    required this.startTime,
-    required this.endTime,
-    required this.content,
-    required this.date,
-    required this.color,
-    required this.createdAt,
-  });
 }
