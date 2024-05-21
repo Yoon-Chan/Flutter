31b55ec405d2d103920e4da6f95262f2dbff014a
윤찬 <vsvx13@naver.com>
2024-05-21 16:10:08 +0900

    Feat:[calendar_scheduler] 캘린더 스타일 변경

--- Flutter3.0/calendar_scheduler/lib/main.dart
+++ Flutter3.0/calendar_scheduler/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:calendar_scheduler/screen/home_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async{
+  //플러터 프레임워크가 실행할 준비가 되어 있는지 확인을 하는 절차
+  //원래 runApp을 실행하면 자동으로 실행된다.
+
+  //하지만 runApp 말고 다른 함수를 먼저 실행하고 싶을 때 직접 사용하는 경우가 있다.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
   runApp(
       MaterialApp(
     home: HomeScreen(),
--- Flutter3.0/calendar_scheduler/lib/screen/home_screen.dart
+++ Flutter3.0/calendar_scheduler/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_scheduler/const/color.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -13,13 +14,59 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final defaultDecoration = BoxDecoration(
+        borderRadius: BorderRadius.circular(6.0),
+        border: Border.all(
+            color: Colors.grey[200]!,
+            width: 1.0
+        )
+    );
+
+    final defaultStyle = TextStyle(
+        color: Colors.grey[600]!
+    );
+
     return Scaffold(
       body: SafeArea(
         child: Container(
           child: TableCalendar(
+            locale: 'ko-kr',
+
             focusedDay: DateTime.now(),
             firstDay: DateTime(1800),
             lastDay: DateTime(3000),
+            headerStyle: const HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextStyle: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w700
+              ),
+            ),
+            calendarStyle: CalendarStyle(
+              isTodayHighlighted: true,
+              defaultDecoration: defaultDecoration,
+              weekendDecoration: defaultDecoration,
+              selectedDecoration: defaultDecoration.copyWith(
+                border: Border.all(
+                  color: primaryColor,
+                  width: 1.0
+                )
+              ),
+              defaultTextStyle: defaultStyle,
+              weekendTextStyle: defaultStyle,
+              selectedTextStyle: defaultStyle.copyWith(
+                color: primaryColor
+              ),
+              todayDecoration: defaultDecoration.copyWith(
+                color: primaryColor
+              ),
+              outsideDecoration: defaultDecoration.copyWith(
+                border: Border.all(
+                  color: Colors.transparent
+                )
+              )
+            ),
             onDaySelected: (selectedDay, focusedDay) {
               setState(() {
                 this.selectedDay = selectedDay;
