636bc4f54361650967964df3933ef5dd96b5839c
윤찬 <vsvx13@naver.com>
2024-05-21 14:10:46 +0900

    Feat:[calendar_scheduler] init

---
+++ Flutter3.0/calendar_scheduler/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
